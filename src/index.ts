e8b010be04a181df6f09c7d6ffc67f6a6d8778b8
Guillaume ALAMOME <guillaume.alamome@airbus.com>
2023-02-10 11:51:34 +0100

    DescribableFunction

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
 const name = "Guillaume"
 console.log(`Hello ${name}!`)
+
+interface DescribableFunction {
+    description: string;
+    todo: (someArg: number) => boolean;
+};
+function doSomething(fn: DescribableFunction, toBeChecked: number) {
+    console.log(`${fn.description} with '${toBeChecked}' returned ${fn.todo(toBeChecked)}`);
+}
+
+const myFn = {
+    todo: (someArg: number): boolean => {
+        return someArg > 5;
+    },
+    description: 'checks if arg is greater than 5'
+}
+
+doSomething(myFn, 4);
